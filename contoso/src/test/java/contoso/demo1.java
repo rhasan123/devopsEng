a71df1103b9bc7fddcfff6980ca1d7ca5212f3b6
rhasan <rhasan@lakeheadu.ca>
2021-04-21 18:33:15 -0400

    Updated Code

--- contoso/src/test/java/contoso/demo1.java
+++ contoso/src/test/java/contoso/demo1.java
@@ -4,7 +4,7 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hello world");
+       System.out.println("Hello world2");
 	}
 
 }
